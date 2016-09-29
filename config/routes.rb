ffe428f1c4e1b6f61ffd21a608a1e5e192165c36
Miral Achmed <miral.achmed@gmail.com>
2016-09-29 17:09:00 +0700

    Change demo/download verb routes from 'post' to 'get'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'demo#home'
-  post 'demo/download' => 'demo#download', as: :download
+  get 'demo/download' => 'demo#download', as: :download
   post 'demo/show_options' => 'demo#show_options', as: :show_options
   post 'demo/hide_options' => 'demo#hide_options', as: :hide_options
 end
